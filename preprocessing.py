ae4308660defbfa01e07db235a9f487afee5f9ae
JennyUJenny <jiayin06717@gmail.com>
2026-01-24 16:12:34 -0500

    Create script for the preprosessor

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,34 @@
-# Imports here
+import whisper 
+import subprocess
 
+def extract_audio(video_path, output_path="output.wav"):
+    command = [
+        "ffmpeg",
+        "-i", video_path,
+        output_path
+    ]
+    subprocess.run(command, check=True)
 
 def preprocess_video(video_path):
-    pass
+    # Install whisper 
+    model = whisper.load_model("turbo")
+    
+    #extract audio
+    extract_audio(video_path)  
+    audio = whisper.load_audio("output.wav")
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    detected_language = max(probs, key=probs.get)
+    print(f"Detected language: {detected_language}")
+
+    # transcribe the audio
+    result = model.transcribe("output.wav", language=detected_language)
+
+    # store the recognized text
+    input_text = result["text"]
+
